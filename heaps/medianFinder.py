53230fe1022ff434105c8d7c5eec8e370ae22d0b
yashmodi622 <yashmodi622@gmail.com>
2025-07-18 08:33:03 +0530

    Implement MedianFinder class with methods to add numbers and find the median using heaps

---
+++ heaps/medianFinder.py
@@ -0,0 +1,27 @@
+import heapq
+
+
+class MedianFinder:
+
+    def __init__(self):
+        self.small = []
+        self.large = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small, -num)
+        heapq.heappush(self.large, -heapq.heappop(self.small))
+
+        if len(self.large) > len(self.small):
+            heapq.heappush(self.small, -heapq.heappop(self.large))
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return self.small[0]
+        return (self.small[0] + self.large[0]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+obj = MedianFinder()
+obj.addNum(1)
+obj.addNum(2)
+print(obj.findMedian())
